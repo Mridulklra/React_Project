e821c73eaf3e3932904f08c14f30142f4937587c
Mridul Kalra <mridulkalra700@gmail.com>
2024-07-31 22:36:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Random from './components/random_color';
 import StarRating from './components/star';
 import Image from './components/Image_Slider';
 import QRCode from './components/qr-code';
+import Board from './components/Tic_Tac_toe';
 function App() {
   return (
     <div className="App">
@@ -13,7 +14,9 @@ function App() {
     <Random/> */}
     {/* <StarRating/> */}
     {/* <Image url={"https://picsum.photos/v2/list?page=1&limit=10"} page={"1"} limit={"10"}/> */}
-    <QRCode/>
+    {/* <QRCode/> */}
+    
+    <Board/>
     </div>
   );
 }
